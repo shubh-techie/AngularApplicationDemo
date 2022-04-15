908b9cdebb2401c382df548b5723db0597c50f80
Shubh Prabhat <shubh.techie@gmail.com>
2022-04-14 20:42:20 -0400

    Added api and called api

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'The Angular Dating app';
+  public users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('http://localhost:5000/api/users/all').subscribe(
+      response => this.users=response, 
+      error=> console.log(error)
+          );
+  }
 }
